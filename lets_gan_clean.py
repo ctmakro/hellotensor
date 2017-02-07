ec7db9ef6266dd19a2ebb6bda867e45db65578af
ctmakro <ctmakro@gmail.com>
2017-02-07 23:03:24 +0800

    cleaned ver

---
+++ lets_gan_clean.py
@@ -0,0 +1,285 @@
+from __future__ import print_function
+
+import tensorflow as tf
+
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import *
+from keras.layers import *
+# from keras.optimizers import *
+from keras.utils import np_utils
+import keras
+import keras.backend as K
+
+import math
+import random
+
+import numpy as np
+
+import cv2
+
+batch_size = 32
+nb_classes = 10
+nb_epoch = 200
+eps=1e-11
+
+zed = 32
+
+def cifar():
+    # input image dimensions
+    img_rows, img_cols = 32, 32
+    # the CIFAR10 images are RGB
+    img_channels = 3
+
+    # the data, shuffled and split between train and test sets
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+    print('X_train shape:', X_train.shape)
+    print(X_train.shape[0], 'train samples')
+    print(X_test.shape[0], 'test samples')
+
+    # convert class vectors to binary class matrices
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+
+    X_train /= 255
+    X_test /= 255
+
+    X_train-=0.5
+    X_test-=0.5
+
+    return X_train,Y_train,X_test,Y_test
+print('loading cifar...')
+xt,yt,xv,yv = cifar()
+
+def relu(i):
+    return LeakyReLU(.2)(i)
+
+def bn(i):
+    return BatchNormalization()(i)
+
+def gen2(): # generative network, 2
+    inp = Input(shape=(zed,))
+    i = inp
+    i = Reshape((1,1,zed))(i)
+
+    ngf=24
+
+    def deconv(i,nop,kw,oh,ow,std=1,tail=True,bm='same'):
+        global batch_size
+        i = Deconvolution2D(nop,kw,kw,subsample=(std,std),border_mode=bm,output_shape=(batch_size,oh,ow,nop))(i)
+        if tail:
+            i = bn(i)
+            i = relu(i)
+        return i
+
+    i = deconv(i,nop=ngf*8,kw=4,oh=4,ow=4,std=1,bm='valid')
+    i = deconv(i,nop=ngf*4,kw=4,oh=8,ow=8,std=2)
+    i = deconv(i,nop=ngf*2,kw=4,oh=16,ow=16,std=2)
+    i = deconv(i,nop=ngf*1,kw=4,oh=32,ow=32,std=2)
+
+    i = deconv(i,nop=3,kw=2,oh=32,ow=32,std=1,tail=False) # out : 32x32
+    i = Activation('tanh')(i)
+
+    m = Model(input=inp,output=i)
+    return m
+
+def concat_diff(i):
+    # return i
+    bv = Lambda(lambda x:K.mean(K.abs(x[:] - K.mean(x,axis=0)),axis=-1,keepdims=True))(i)
+    i = merge([i,bv],mode='concat')
+    return i
+
+def dis2(): # discriminative network, 2
+    # inp = Input(shape=(None,None,3))
+    inp = Input(shape=(32,32,3))
+    i = inp
+
+    ndf=24
+
+    def conv(i,nop,kw,std=1,usebn=True,bm='same'):
+        i = Convolution2D(nop,kw,kw,border_mode=bm,subsample=(std,std))(i)
+        if usebn:
+            i = bn(i)
+        i = relu(i)
+        return i
+
+    i = conv(i,ndf*1,4,std=2,usebn=False)
+    i = concat_diff(i)
+    i = conv(i,ndf*2,4,std=2)
+    i = concat_diff(i)
+    i = conv(i,ndf*4,4,std=2)
+    i = concat_diff(i)
+    i = conv(i,ndf*8,4,std=2)
+    i = concat_diff(i)
+
+    # 1x1
+    i = Convolution2D(1,2,2,border_mode='valid')(i)
+
+    i = Activation('linear',name='conv_exit')(i)
+    i = Activation('sigmoid')(i)
+
+    i = Reshape((1,))(i)
+
+    m = Model(input=inp,output=i)
+    return m
+
+print('generating G...')
+gm = gen2()
+gm.summary()
+
+print('generating D...')
+dm = dis2()
+dm.summary()
+
+def gan(gm,dm):
+    # initialize a GAN trainer
+
+    # this is the fastest way to train a GAN in Keras
+    # two models are updated simutaneously in one pass
+
+    noise = Input(shape=(zed,))
+    real_image = Input(shape=(32,32,3))
+
+    generated = gm(noise)
+    gscore = dm(generated)
+    rscore = dm(real_image)
+
+    def log_eps(i):
+        return K.log(i+1e-11)
+
+    # single side label smoothing: replace 1.0 with 0.9
+    dloss = - K.mean(log_eps(1-gscore) + .1 * log_eps(1-rscore) + .9 * log_eps(rscore))
+    gloss = - K.mean(log_eps(gscore))
+
+    Adam = tf.train.AdamOptimizer
+
+    lr,b1 = 1e-4,.2 # otherwise won't converge.
+    optimizer = Adam(lr,beta1=b1)
+
+    grad_loss_wd = optimizer.compute_gradients(dloss, dm.trainable_weights)
+    update_wd = optimizer.apply_gradients(grad_loss_wd)
+
+    grad_loss_wg = optimizer.compute_gradients(gloss, gm.trainable_weights)
+    update_wg = optimizer.apply_gradients(grad_loss_wg)
+
+    def get_internal_updates(model):
+        # get all internal update ops (like moving averages) of a model
+        inbound_nodes = model.inbound_nodes
+        input_tensors = []
+        for ibn in inbound_nodes:
+            input_tensors+= ibn.input_tensors
+        updates = [model.get_updates_for(i) for i in input_tensors]
+        return updates
+
+    other_parameter_updates = [get_internal_updates(m) for m in [dm,gm]]
+    # those updates includes batch norm.
+
+    print('other_parameter_updates for the models(mainly for batch norm):')
+    print(other_parameter_updates)
+
+    train_step = [update_wd, update_wg, other_parameter_updates]
+    losses = [dloss,gloss]
+
+    learning_phase = tf.get_default_graph().get_tensor_by_name(
+        'keras_learning_phase:0')
+
+    def gan_feed(sess,batch_image,z_input):
+        # actual GAN trainer
+        nonlocal train_step,losses,noise,real_image,learning_phase
+
+        res = sess.run([train_step,losses],feed_dict={
+        noise:z_input,
+        real_image:batch_image,
+        learning_phase:True,
+        # Keras layers needs to know whether
+        # this run is training or testring (you know, batch norm and dropout)
+        })
+
+        loss_values = res[1]
+        return loss_values #[dloss,gloss]
+
+    return gan_feed
+
+print('generating GAN...')
+gan_feed = gan(gm,dm)
+
+print('Ready. enter r() to train')
+
+def r(ep=1000,noise_level=.01):
+    sess = K.get_session()
+
+    for i in range(ep):
+        noise_level *= 0.99
+        print('---------------------------')
+        print('iter',i,'noise',noise_level)
+        batches = 1
+        total = batch_size*batches
+        indices = np.random.choice(50000,total,replace=False)
+
+        # random sample from cifar
+        subset_cifar = np.take(xt,indices,axis=0)
+        subset_cifar += np.random.normal(loc=0.,scale=noise_level,size=subset_cifar.shape)
+        # add gaussian term
+        z_input = np.random.normal(loc=0.,scale=1.,size=(total,zed))
+
+        # train for one step
+        losses = gan_feed(sess,subset_cifar,z_input)
+        print('dloss:{:6.4f} gloss:{:6.4f}'.format(losses[0],losses[1]))
+
+        if i==ep-1 or i % 10==0: show()
+
+def autoscaler(img):
+    limit = 400.
+    # scales = [0.1,0.125,1./6.,0.2,0.25,1./3.,1./2.] + range(100)
+    scales = np.hstack([1./np.linspace(10,2,num=9), np.linspace(1,100,num=100)])
+
+    imgscale = limit/float(img.shape[0])
+    for s in scales:
+        if s>=imgscale:
+            imgscale=s
+            break
+
+    img = cv2.resize(img,dsize=(int(img.shape[1]*imgscale),int(img.shape[0]*imgscale)),interpolation=cv2.INTER_NEAREST)
+
+    return img,imgscale
+
+def flatten_multiple_image_into_image(arr):
+    import cv2
+    num,uh,uw,depth = arr.shape
+
+    patches = int(num+1)
+    height = int(math.sqrt(patches)*0.9)
+    width = int(patches/height+1)
+
+    img = np.zeros((height*uh+height, width*uw+width, 3),dtype='float32')
+
+    index = 0
+    for row in range(height):
+        for col in range(width):
+            if index>=num-1:
+                break
+            channels = arr[index]
+            img[row*uh+row:row*uh+uh+row,col*uw+col:col*uw+uw+col,:] = channels
+            index+=1
+
+    img,imgscale = autoscaler(img)
+
+    return img,imgscale
+
+def show(save=False):
+    i = np.random.normal(loc=0.,scale=1.,size=(batch_size,zed))
+    gened = gm.predict([i])
+
+    gened *= 0.5
+    gened +=0.5
+
+    im,ims = flatten_multiple_image_into_image(gened)
+    cv2.imshow('gened scale:'+str(ims),im)
+    cv2.waitKey(1)
+
+    if save!=False:
+        cv2.imwrite(save,im*255)

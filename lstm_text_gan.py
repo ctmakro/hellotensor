f94810920565e9c957f7c2335e9bb122b15cbf3e
ctmakro <ctmakro@gmail.com>
2017-03-04 17:51:18 +0800

    Merge branch 'master' of https://github.com/ctmakro/hellotensor
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- lstm_text_gan.py
+++ lstm_text_gan.py
@@ -5,26 +5,24 @@ from keras.utils.data_utils import get_file
 import numpy as np
 import random
 import sys
-
-# from tensorflow_boilerplate import *
 import canton as ct
 import tensorflow as tf
 
 zed = 32
-time_steps = 1024
+time_steps = 16
 
-def char2eight(n):
-    binary = "{0:b}".format(n)
-    rep = np.zeros((8,),dtype='float32')
-    for i in range(len(binary)): # 0..2
-        rep[i+8-len(binary)] = 1 if binary[i] == '1' else 0
-    return rep
-
-def eight2char(e):
-    out = 0
-    for i in range(len(e)):
-        out += (1 if e[7-i]>0.5 else 0) * 2**i
-    return int(out)
+# def char2eight(n):
+#     binary = "{0:b}".format(n)
+#     rep = np.zeros((8,),dtype='float32')
+#     for i in range(len(binary)): # 0..2
+#         rep[i+8-len(binary)] = 1 if binary[i] == '1' else 0
+#     return rep
+#
+# def eight2char(e):
+#     out = 0
+#     for i in range(len(e)):
+#         out += (1 if e[7-i]>0.5 else 0) * 2**i
+#     return int(out)
 
 def one_hot(tensor,dims):
     onehot = np.zeros((len(tensor),dims),dtype='float32')
@@ -52,17 +50,10 @@ def textdata():
 
     # convert into ascii
     asc = to_asc(text)
-
-    # convert into 8dim
-    # for i in range(length):
-    #     onehot[i] = char2eight(asc[i])
-
-    # convert into onehot
     onehot = one_hot(asc,256)
-
     return text,onehot
 
-text,corpus = textdata()
+text, corpus = textdata() # the string and the one_hot encoded
 print('corpus loaded. corpus[0]:',corpus[0])
 
 def mymodel_builder():
@@ -74,11 +65,13 @@ def mymodel_builder():
         # (batch, time_steps, 512)
         shape = tf.shape(i)
         b,t,d = shape[0],shape[1],shape[2]
+
         ending_state = i[:,t-1,:]
 
         i = d1(i)
-        i = tf.nn.elu(i)
+        i = ct.Act('elu')(i)
         i = d2(i)
+        i = ct.Act('softmax')(i)
 
         if starting_state is None:
             return i
@@ -90,27 +83,32 @@ def mymodel_builder():
 mymodel = mymodel_builder()
 
 def feed_gen():
-    x = tf.placeholder(tf.float32, shape=[None, None, corpus.shape[1]])
-    y = mymodel(x)
-    gt = tf.placeholder(tf.float32, shape=[None, None, corpus.shape[1]])
-    loss = ct.mean_softmax_cross_entropy(y,gt)
+    input_text = tf.placeholder(tf.float32,
+        shape=[None, None, corpus.shape[1]]) # [batch, timesteps, 256]
+
+    xhead = input_text[:,:-1] # [batch, 0:timesteps-1, 256]
+    gt = input_text[:,1:] # [batch, 1:timesteps, 256]
+    y = mymodel(xhead) # [batch, 1:timesteps, 256]
+
+    loss = ct.cross_entropy_loss(y,gt)
 
     train_step = tf.train.AdamOptimizer(1e-3).minimize(
         loss,var_list=mymodel.get_weights())
 
-    def feed(minibatch,labels):
-        nonlocal train_step,loss,x,gt
+    def feed(minibatch):
+        nonlocal train_step,loss,input_text
         sess = ct.get_session()
-        res = sess.run([loss,train_step],feed_dict={x:minibatch,gt:labels})
+        res = sess.run([loss,train_step],feed_dict={input_text:minibatch})
         return res[0]
 
     starting_state = tf.placeholder(tf.float32, shape=[None, None])
-    stateful_y, ending_state = mymodel(x,starting_state=starting_state)
+    stateful_y, ending_state = mymodel(input_text,starting_state=starting_state)
 
     def predict(st,i):
         # stateful, to enable fast generation.
         sess = ct.get_session()
-        res = sess.run([stateful_y,ending_state],feed_dict={x:i,starting_state:st})
+        res = sess.run([stateful_y,ending_state],
+            feed_dict={input_text:i,starting_state:st})
         return res
 
     return feed,predict
@@ -118,7 +116,6 @@ def feed_gen():
 feed,predict = feed_gen()
 
 sess = ct.get_session()
-sess.run(tf.variables_initializer(mymodel.get_weights()))
 sess.run(tf.global_variables_initializer())
 
 def r(ep=100):
@@ -133,10 +130,8 @@ def r(ep=100):
 
         minibatch = corpus[j:j+mbl]
         minibatch.shape = [batch_size, time_steps, corpus.shape[1]]
-        labels = corpus[j+1:j+mbl+1]
-        labels.shape = [batch_size,time_steps,corpus.shape[1]]
 
-        loss = feed(minibatch,labels)
+        loss = feed(minibatch)
         print('loss:',loss)
 
         if i%100==0 : pass#show2()
@@ -147,31 +142,23 @@ def softmax(x):
 
 import sys
 def show2(length=400):
-    t = 97
+    code = 97 # last character generated
     starting_state = np.zeros((1,256),dtype='float32')
     for i in range(length):
-        # convert into ascii
-        # asc = to_asc(t[-time_steps:])
-        asc = t
-        hot = one_hot_int(asc,256)
-        hot.shape = (1,)+hot.shape
+        hot = one_hot_int(code,256)
+        hot.shape = (1,) + hot.shape
         #print(hot.shape)
 
         [stateful_y,ending_state] = predict(starting_state,hot)
         starting_state = ending_state
 
         res = stateful_y[0,0] # choose the last dimension
-        dist = softmax(res) # do the softmath
-        dist = (dist - np.mean(dist) > 0) * dist # pick the greater ones
-        dist = dist / np.sum(dist) # normalize sum to 1
+        dist = res # softmax(res) # do the softmath
         code = np.random.choice(256, p=dist)
-        # code = np.argmax(dist)
 
-        #print(code)
         char = chr(code)
-        t=code
-
         sys.stdout.write( '%s' % char )
-        if i%10==0 : sys.stdout.flush()
+        if i%10==0:
+            sys.stdout.flush()
     sys.stdout.flush()
     print('')

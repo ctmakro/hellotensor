06a58da87da0c376f26b5b4fb42243486c7c1f1d
ctmakro <ctmakro@gmail.com>
2017-03-29 16:29:31 +0800

    UTF-8 stateful decoder finished. Can decode neural network output into UTF-8 now without errors!

--- gru_text_generation_2.py
+++ gru_text_generation_2.py
@@ -7,12 +7,9 @@ import tensorflow as tf
 
 time_steps = 16
 
-def get_text_data():
-    # from keras.utils.data_utils import get_file
-    # path = get_file('nietzsche.txt', origin="https://s3.amazonaws.com/text-datasets/nietzsche.txt")
-    # text = open(path).read()
+def get_text_data(filename):
     import codecs
-    with open('oanc_partial.txt','rb') as f:
+    with open(filename,'rb') as f:
         text = f.read()
     length = len(text)
     print('got corpus length:', length)
@@ -105,7 +102,7 @@ def feed_gen():
 
 def r(ep=100):
     length = len(corpus)
-    batch_size = 1
+    batch_size = 256
     mbl = time_steps * batch_size
     sr = length - mbl - time_steps - 2
     for i in range(ep):
@@ -121,26 +118,75 @@ def r(ep=100):
 
         if i%100==0 : pass#show2()
 
+class utf8statemachine:
+    # byte(int)s in. character out (when ready). raises error when illegal.
+    def __init__(self):
+        self.state=0
+        self.buffer = []
+    def flush(self):
+        char = str(bytes(self.buffer),'utf-8')
+        self.buffer = []
+        return char
+    def bytein(self,b):
+        # assume b is uint.
+        if self.state==0: # normal mode
+            if b & 0b10000000 == 0:# if first bit is 0
+                self.buffer.append(b)
+                return self.flush()
+            if b & 0b11110000 == 0b11100000: # if starts with 1110
+                self.state=2
+                self.buffer.append(b)
+                return None
+            if b & 0b11100000 == 0b11000000: # if starts with 110
+                self.state=1
+                self.buffer.append(b)
+                return None
+            raise NameError('byte should start with 0b0 or 0b110 or 0b1110')
+        
+        if self.state>0:
+            if b & 0b11000000 == 0b10000000:# if starts with 10
+                self.state -=1
+                self.buffer.append(b)
+            else:
+                raise NameError('byte should start with 0b10')
+            
+            if self.state==0:
+                return self.flush()
+            return None       
+            
 def show2(length=400):
-    import sys
-    asc_buf = np.fromstring('a',dtype='uint8').reshape(1,1)
+    import sys,os
+    asc_buf = np.fromstring('\n',dtype='uint8').reshape(1,1)
     starting_state = None
-
+    sm = utf8statemachine()
+    errors = 0
     # sequentially generate text out of the GRU
     for i in range(length):
         stateful_y, ending_state = predict(starting_state,asc_buf)
-        starting_state = ending_state
 
         dist = stateful_y[0,0] # last dimension is the probability distribution
-        code = np.random.choice(256, p=dist)
-        char = chr(code)
-        sys.stdout.write( '%s' % char )
+        code = np.random.choice(256, p=dist) # sample a byte from distribution
+
+        try:
+            result = sm.bytein(code) # put in utf8 state machine
+            if result is not None: # if the state machine spit out a character
+                sys.stdout.write(result) # write to stdout
+                
+            # accept the result if no utf-8 decoding error detected
+            asc_buf[0,0] = code
+            starting_state = ending_state
+        except NameError: # if decoding error
+            # sys.stdout.write('e')
+            errors += 1
+            # don't accept the results, try sample again next iteration
+        
         if i%10==0:
             sys.stdout.flush()
-
-        asc_buf[0,0] = code
+            pass
+       
     sys.stdout.flush()
     print('')
+    print('total UTF-8 decoding errors:',errors)
 
 # bullshit analyzer
 def bsa(text):
@@ -160,13 +206,18 @@ def bsa(text):
 
     print('simplified:',simplified)
 
-text = get_text_data() # the string
-corpus = np.fromstring(text,dtype='uint8') # the bytes
-print('corpus loaded. corpus[0]:',corpus[0], 'text[0]:',text[0])
+argv = sys.argv
+ 
+if len(argv)<2:
+    print('(Error)please provide a filename as the first argument. The file should be in UTF-8 encoding, without BOM.')
+else:
+    text = get_text_data(argv[1]) # the string
+    corpus = np.fromstring(text,dtype='uint8') # the bytes
+    print('corpus loaded. corpus[0]:',corpus[0], 'text[0]:',text[0])
 
-model = model_builder()
+    model = model_builder()
 
-feed, predict, loss_stats = feed_gen()
+    feed, predict, loss_stats = feed_gen()
 
-sess = ct.get_session()
-sess.run(tf.global_variables_initializer())
+    sess = ct.get_session()
+    sess.run(tf.global_variables_initializer())

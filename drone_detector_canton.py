835fc30dc92387adb6ad364144db2c320e5e6bc7
ctmakro <ctmakro@gmail.com>
2017-03-29 18:03:00 +0800

    not very successful, reconsidering

--- drone_detector_canton.py
+++ drone_detector_canton.py
@@ -54,29 +54,42 @@ def detector():
 
 def detector2():
     c = Can()
-    c.add(Conv2D(1,32,k=5,std=2)) # 32
+    # in : 96
+    c.add(Conv2D(1,16,k=5,std=2,padding='VALID')) # 92/2 = 46
     c.add(Act('lrelu'))
-    c.add(Conv2D(32,32,k=5,std=2)) # 16
+    c.add(Conv2D(16,16,k=5,std=2,padding='VALID')) # 42/2 = 21
     c.add(Act('lrelu'))
-    c.add(Conv2D(32,32,k=5,std=2)) # 8
+    c.add(Conv2D(16,16,k=5,std=2,padding='VALID')) # 17/2 = 9
     c.add(Act('lrelu'))
-    c.add(Conv2D(32,1,k=3,std=1)) # 8
+    c.add(Conv2D(16,32,k=3,std=1,padding='VALID')) # 9-2 = 7
+    c.add(Act('lrelu'))
+    c.chain()
+    return c
+    
+# split detector and classifier into 2 network
+def classify(k):
+    c=Can()
+    c.add(k)
+    c.add(Conv2D(32,1,k=1))
     c.add(Act('sigmoid'))
     c.chain()
     return c
 
-tec = detector()
+det = detector2()
+tec = classify(det)
 tec.summary()
 
 def trainer():
     x,gt = ct.ph([None,None,1]), ct.ph([None,None,1])
-    y = tec(x)
+    y = tec(x) 
+    decay = tf.reduce_mean([tf.reduce_mean(w**2) for w in tec.get_weights()]) * 1e-4
+
     loss = ct.binary_cross_entropy_loss(y,gt)
     lr = tf.Variable(1e-3)
 
     print('connecting optimizer...')
     opt = tf.train.AdamOptimizer(lr)
-    train_step = opt.minimize(loss,var_list=tec.get_weights())
+    train_step = opt.minimize(loss+decay,var_list=tec.get_weights())
 
     def feed(xin,yin,ilr):
         sess = ct.get_session()
@@ -94,8 +107,10 @@ def r(ep=10,lr=1e-3):
         for j in range(len(xt)//bs):
             mbx = xt[j*bs:(j+1)*bs]
             mby = yt[j*bs:(j+1)*bs]
+            mby = mby[:,2:2+7,2:2+7,:] # crop the center 7x7 from 12x12
             loss = feed(mbx,mby,lr)
             print(j*bs,'loss:',loss)
+        show()
 
 def show(): # evaluate result on validation set
     from vis import autoscaler,batch_image_array
